c60a2b6fa1d95e7d3f6e27761b884a3ff8ea4ff0
Piotr Łączykowski <piotr.laczykowski@iver.pl>
2024-06-07 16:33:38 +0200

    add a script version which can be used at startup

---
+++ update-pc-script-auto.py
@@ -0,0 +1,17 @@
+import sys
+from utilities import *
+from dell import *
+
+if __name__ == "__main__":
+    if run_as_admin():
+        dell()
+    # Update Winget
+        winget_upgrade()
+    # Check if Chocolatey (choco) is installed and perform the necessary actions
+        choco_upgrade()
+    # Update Windows
+        windows_update()
+        # End of the script
+        print("Script execution completed.")
+    else:
+        sys.exit(0)  # Exit if user declines admin privileges
