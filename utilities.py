823090b4ad6d5590dea555cbe36581600671ebad
Piotr Łączykowski <piotr.laczykowski@iver.pl>
2024-06-07 19:18:22 +0200

    delete unknown from winget

--- utilities.py
+++ utilities.py
@@ -94,7 +94,7 @@ def install_program(installer_path):
 def winget_upgrade():
     # Upgrade all installed packages using winget
     try:
-        subprocess.run(["winget", "upgrade", "--all", "--accept-source-agreements", "--accept-package-agreements", "-u", "--allow-reboot"], check=True,)
+        subprocess.run(["winget", "upgrade", "--all", "--accept-source-agreements", "--accept-package-agreements", "--allow-reboot"], check=True,)
         print("All installed packages upgraded successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error upgrading packages with winget: {e}")
